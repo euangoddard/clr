1334e025d546b8917f7a5f2e69bf5a1d543f044b
Euan Goddard <euangoddard@2degreesnetwork.com>
2015-08-17 13:33:39 +0100

    Added speech to colour selection

--- src/js/main.js
+++ src/js/main.js
@@ -4,18 +4,30 @@ var domready = require('domready');
 var each = require('lodash.foreach');
 var to_array = require('lodash.toarray');
 
+var speech = require('./speech');
+
 domready(function () {
-	var circles = document.querySelectorAll('circle');
-	each(circles, function (circle) {
-		circle.addEventListener('click', select_circle, false);
-	});
+    var circles = document.querySelectorAll('circle');
+    each(circles, function (circle) {
+        circle.addEventListener('click', select_circle, false);
+    });
+    speech.language = 'en-GB';
 });
 
 
 var select_circle = function (event) {
-	var circle = this;
-	var class_list = document.body.classList;
-	class_list.remove.apply(class_list, to_array(class_list));
-	class_list.add.apply(class_list, to_array(circle.classList));
-	document.querySelector('p').textContent = to_array(circle.classList).join(' ');
+    var circle = this;
+    var class_list = document.body.classList;
+    var colour_name = to_array(circle.classList)[0];
+    
+    if (class_list.contains(colour_name)) {
+        return;
+    }
+    
+    class_list.remove.apply(class_list, to_array(class_list));
+    
+    class_list.add(colour_name);
+    var colour_name_label = document.getElementById('colour-name');
+    colour_name_label.textContent = colour_name;
+    speech.say(colour_name);
 };
---
+++ src/js/speech.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var is_available = ('speechSynthesis' in window);
+
+var preferred_language = null;
+
+var service = {};
+
+if (is_available) {
+	var voices = [];
+	
+	speechSynthesis.onvoiceschanged = function (event) {
+	    voices = speechSynthesis.getVoices();
+	};
+	
+	var get_voice_for_language = function (language) {
+	    var voice_matches_language = function (v) { return v.lang === language};
+	    var voices_for_lang = voices.filter(voice_matches_language);
+	    var preferred_voice = voices_for_lang.slice(-1)[0];
+	    if (!preferred_voice) {
+	        console.warn('Could not select voice for "%s"', language);
+	    }
+	    return preferred_voice;
+	};
+	
+	service.say =  function (text) {
+        var utterance = new SpeechSynthesisUtterance(text);
+        var voice = get_voice_for_language(preferred_language);
+        if (voice) {
+            utterance.voice = voice;
+        } else {
+            utterance.lang = preferred_language;
+        }
+        speechSynthesis.speak(utterance);
+    };
+}
+
+Object.defineProperty(service, 'language', {
+    get: function () { return preferred_language; },
+    set: function (language) { preferred_language = language; },
+    enumerable: true,
+    configurable: true
+});
+
+module.exports = service;
